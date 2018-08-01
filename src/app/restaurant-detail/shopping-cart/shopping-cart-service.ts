c6e6caaf0a3f961e7b13f68f7b6cac4c254938b6
Leonardo Faria <leonardofarias1806@gmail.com>
2018-07-31 22:39:17 -0300

    - troca para moeda brasileira intl(inclusão polyfills e module), utilização de provider estendido; - criação da ordem de compra [routerLink]="['/order]"; - inclusão de arquivos do comportamento do carrinho;

---
+++ src/app/restaurant-detail/shopping-cart/shopping-cart-service.ts
@@ -0,0 +1,34 @@
+import { CartItem } from "./cart-item.model";
+import { MenuItem } from "../menu-item/menu-item.model";
+import { PreActivation } from "../../../../node_modules/@angular/router/src/router";
+
+export class ShoppingCartService {
+    
+    items: CartItem[] = []
+
+    clear(){
+        this.items = []
+    }
+
+    addItem(item:MenuItem){
+        let foundItem = this.items.find((mItem)=> mItem.menuItem.id === item.id)
+        if(foundItem){
+            foundItem.quantity = foundItem.quantity + 1
+        }else{
+            this.items.push(new CartItem(item))
+        }
+    }
+
+    removeItem(item:CartItem){
+        this.items.splice(this.items.indexOf(item),1)
+    }
+
+    total(): number{
+        // no map troca o objeto item pelo valor do item
+        // no reduce adiciona o valor anterior o novo valor
+        return this.items
+            .map(item => item.value())
+            .reduce((prev,value) => prev + value, 0);
+    }
+
+}
