b3255b7f0fed6a8a9140fd7941efe7e9fbd9b7c9
Leonardo Faria <leonardofarias1806@gmail.com>
2018-10-25 21:42:48 -0300

    - validador customizado

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -6,7 +6,7 @@ import { CartItem } from '../restaurant-detail/shopping-cart/cart-item.model';
 import { shimContentAttribute } from '../../../node_modules/@angular/platform-browser/src/dom/dom_renderer';
 import { Order, OrdemItem } from './order.model';
 import { Router } from '@angular/router';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, AbstractControl } from '@angular/forms';
 
 @Component({
   selector: 'mt-order',
@@ -40,7 +40,20 @@ export class OrderComponent implements OnInit {
       number: this.formBuilder.control('', [Validators.required, Validators.pattern(this.numberPattern)]),
       optionalAddress: this.formBuilder.control(''),
       paymentOption: this.formBuilder.control('', [Validators.required])
-    })
+    }, {validator: OrderComponent.equalsTo})
+  }
+
+  static equalsTo(group: AbstractControl): {[key:string]: boolean} {
+    const email = group.get('email')
+    const emailConfirmation = group.get('emailConfirmation')
+    if(!email || !emailConfirmation){
+      return undefined
+    }
+
+    if(email.value !== emailConfirmation.value){
+      return {emailsNotMatch:true}
+    }
+    return undefined
   }
 
   itemsValue(): number {
