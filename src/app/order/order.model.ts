c8e6dad85f78886e52462becefd09d5a05df5bb3
Leonardo Faria <leonardofarias1806@gmail.com>
2018-12-04 21:44:41 -0200

    - estratégia hash para melhor adaptação da aplicação

---
+++ src/app/order/order.model.ts
@@ -0,0 +1,15 @@
+class Order {
+    constructor(
+        public address: string,
+        public number: number,
+        public optionalAddress: string,
+        public paymentOption: string,
+        public orderItems: OrdemItem[] = []
+    ){}
+}
+
+class OrdemItem {
+    constructor(public quantity: number, public menuId: string){}
+}
+
+export {Order, OrdemItem}
